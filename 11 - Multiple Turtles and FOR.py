b4624e0fb09f396c219a77b622c6a8578f6f5a79
stefan-scott <scottst@spsd.sk.ca>
2023-11-21 09:43:06 -0600

    demo done

--- 11 - Multiple Turtles and FOR.py
+++ 11 - Multiple Turtles and FOR.py
@@ -15,16 +15,35 @@ martha = turtle.Turtle()
 martha.pensize(4)
 martha.color("purple")
 
+turtle_list = [] #empty list, to store turtles in
+color_list = ["red", "blue", "orange", "green"]
+
+turtle_list.append(francis) #add our Francis turtle onto the end of the list
+turtle_list.append(martha) #same, but for Martha
+turtle_list.append(turtle.Turtle()) #make a third generic turtle
+
+# Now, loop through our list and give some instructions.
+turtle_list[0].left(120)
+turtle_list[1].left(240)
+
+color_index = 0
+for distance in range(50, 10, -1):  #[50, 48, 46, ..., 14, 12]
+    for t in turtle_list:
+        t.color(color_list[color_index])
+        t.speed(0)
+        t.forward(distance)
+        t.right(20)
+    #update the color for next time:
+    color_index = color_index + 1
+    if color_index > 3:
+        color_index = 0
 
 
 
 
-
-
-
-#    loop variable 
-# for       name      in ["Joe", "Amy", "Brad", "Zuki"]:
-#     print("Hello there, " + name)
+ 
+# my_list = ["Joe", "Amy", "Brad", "Zuki"]
+# #            0      1      2       3
 
 
  
