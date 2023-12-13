2ec41124f3e0f70ddff968a50ac6462e3fc74888
stefan-scott <scottst@spsd.sk.ca>
2023-12-13 09:53:21 -0600

    Demo is Complete

--- 20 - Microbit LED Functions.py
+++ 20 - Microbit LED Functions.py
@@ -40,15 +40,50 @@ def plot(x,y):
 def unplot(x,y):
     #turn off a pixel at (x,y)
     set_pixel(x,y,0)
-
-
     
 def print_grid():
     #print out grid in an easy-to-read format
     for row in grid:
         print(row)
-    
-    
+        
+# Main Code Starts Here
+
+def set_all(intensity):
+    # fill all pixels to level "intensity"
+    for x in range(5):  #x → 0, 1, 2, 3, 4
+        for y in range(5): #y → 0, 1, 2, 3, 4
+            queue_pixel(x,y,intensity)
+    show_grid()
+
+# Fading Effect - Animation
+for cycles in range(3):
+    for brightness in range(10):  #0-9
+        set_all(brightness)
+        time.sleep(0.05)
+
+    for brightness in range(8,0,-1):   #8-1
+        set_all(brightness)
+        time.sleep(0.05)
+        
+# Game Mechanics - driving a player around
+set_all(0) #clear the screen
+player_x = 2
+player_y = 4
+
+plot(player_x, player_y)
+
+while True:
+    if microbit.button_a.was_pressed():
+        if player_x > 0:  #if there's room to move
+            unplot(player_x, player_y)
+            player_x = player_x - 1
+            plot(player_x, player_y)
+            
+    if microbit.button_b.was_pressed():
+        if player_x < 4: #if there's room to move
+            unplot(player_x, player_y)
+            player_x += 1
+            plot(player_x, player_y)
     
 # Recap: Built-in vs Custom
 # microbit.display.show(microbit.Image.HAPPY)   #BUILT-IN
